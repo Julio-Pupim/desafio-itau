4ec4fee220ee6c2d1f5034650457224ae7c8c993
julio <julio.minellamatheus@gmail.com>
2025-03-22 19:53:46 -0300

    feat: criando a controller endpoints post e delete

---
+++ src/main/java/com/juliopupim/desafios/itau/controller/TransacaoController.java
@@ -0,0 +1,42 @@
+package com.juliopupim.desafios.itau.controller;
+
+import com.juliopupim.desafios.itau.domain.Transacao;
+import com.juliopupim.desafios.itau.domain.dto.TransacaoRequestDTO;
+import com.juliopupim.desafios.itau.service.TransacaoService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class TransacaoController {
+
+  private final TransacaoService transacaoService;
+
+  @PostMapping("/transacao")
+  public ResponseEntity<Transacao> salvarTransacao(
+      @RequestBody @Valid TransacaoRequestDTO transacaoDTO) {
+    try {
+      transacaoService.save(transacaoDTO);
+      return ResponseEntity.status(HttpStatus.CREATED).build();
+    } catch (IllegalArgumentException ex) {
+      log.error("O objeto enviado está inválido: {} {}",
+          transacaoDTO, ex.toString());
+      return ResponseEntity.unprocessableEntity().build();
+    }
+  }
+
+  @DeleteMapping("/transacao")
+  public ResponseEntity<Void> deleteTransacao() {
+    transacaoService.deleteAll();
+    return ResponseEntity.ok().build();
+  }
+
+}
---
+++ src/test/java/com/juliopupim/desafios/itau/controller/TransacaoControllerTest.java
@@ -0,0 +1,99 @@
+package com.juliopupim.desafios.itau.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.juliopupim.desafios.itau.domain.Transacao;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TransacaoControllerTest {
+
+  private MockMvc mockMvc;
+
+
+  @Autowired
+  private TransacaoController transacaoController;
+
+  private JacksonTester<Transacao> jackson;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @BeforeEach
+  public void setUp() {
+    JacksonTester.initFields(this, objectMapper);
+    mockMvc = MockMvcBuilders.standaloneSetup(transacaoController).build();
+  }
+
+
+  @Test
+  public void deveSalvarTransacao() throws Exception {
+    Transacao transacao = Transacao.of(BigDecimal.TEN, OffsetDateTime.now().minusDays(1));
+    MockHttpServletResponse response = mockMvc.perform(
+            MockMvcRequestBuilders.post("/transacao")
+                .contentType(MediaType.APPLICATION_JSON).content(jackson.write(transacao)
+                    .getJson()))
+        .andReturn()
+        .getResponse();
+
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+    Assertions.assertThat(response.getContentAsString()).isEmpty();
+
+  }
+
+  @Test
+  public void deveLancarErro400QuandoJsonInvalido() throws Exception {
+    String jsonInvalido = "{ \"valor\": 10.5, \"dataHora\": \"2024-03-20T10:00:00Z\"";
+    MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.post("/transacao")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonInvalido))
+        .andReturn()
+        .getResponse();
+
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    Assertions.assertThat(response.getContentAsString()).isEmpty();
+
+  }
+
+  @Test
+  public void deveLancarErro422() throws Exception {
+    String jsonInvalido = "{ \"valor\": -10.0, \"dataHora\": \"2024-03-20T10:00:00Z\"}";
+
+    MockHttpServletResponse response = mockMvc.perform(
+            MockMvcRequestBuilders.post("/transacao")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonInvalido))
+        .andReturn()
+        .getResponse();
+
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+    Assertions.assertThat(response.getContentAsString()).isEmpty();
+
+  }
+
+  @Test
+  public void deleteTransacao() throws Exception {
+    MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.delete("/transacao"))
+        .andReturn()
+        .getResponse();
+
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    Assertions.assertThat(response.getContentAsString()).isEmpty();
+
+  }
+}
